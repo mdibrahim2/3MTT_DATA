f36fdbd450fc848382446165ee803657d6927b2e
MUHAMMAD DINI IBRAHIM <muhammaddibrahim2@gmail.com>
2024-04-01 08:47:26 -0700

    Update and rename week3_assessment.ipynb to week3_assessment.py

---
+++ week3_assessment.py
@@ -0,0 +1,8 @@
+##importing pandas LIbrary
+import pandas as pd
+import numpy as np
+
+## reading the excell file from external source
+df = pd.read_excel(r"C:\Users\MDI\Desktop\DATA ANALYSIS\GALADI.xlsx")
+##outputing tthe excell dataset
+print(df)
